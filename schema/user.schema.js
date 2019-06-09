93e8e44ec93bc8c47582547e92ae07123369ab82
Mohammed Akhil <akhilmohammed05@gmail.com>
2019-06-09 22:53:30 +0530

    update user schema

--- schema/user.schema.js
+++ schema/user.schema.js
@@ -3,14 +3,14 @@ module.exports = {
 			firstName VARCHAR(20), 
 			lastName VARCHAR(20), 
 			fatherName VARCHAR(20), 
-			pan VARCHAR(20), 
-			dob VARCHAR(20),
+			panNumber VARCHAR(20) UNIQUE, 
+			dateOfBirth VARCHAR(20),
 			gender VARCHAR(20),
 			address VARCHAR(254),
 			profileImage VARCHAR(100), 
 			email VARCHAR(254) UNIQUE,
 			created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 			updated_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-			PRIMARY KEY(id),
+			PRIMARY KEY(id));
 		`,
 };
